0ff18cbad77181ccb48ed01ad85462d4301b0b37
ganidilbert <124190842+ganidilbert@users.noreply.github.com>
2023-03-03 11:57:40 -0600

    Add files via upload

---
+++ traffic_sign_facotry/res/res/rat.js
@@ -0,0 +1,15 @@
+var rats=
+[
+{
+  'Better':       ['Worser','Greater','Smaller'],
+  'be':           ['be','not to be','having fun'],
+  'a Samurai':    ['a Samurai','no one','a Gardener','yourself'],
+  'in a Garden':  ['in a Garden','in the Heaven','nowhere','at War'],
+  'than':         ['than'],
+},
+{
+  'You':    ['I','Grandma','Curiosity','You'],
+  'killed': ['revived','painted','made to purr','killed'],
+  'a Rat':  ['a Samurai','the Cat','drivers','someone','a Rat'],
+}
+];
---
+++ traffic_sign_facotry/res/res/ratKing.js
@@ -0,0 +1,6 @@
+var ratKing = [
+  'Better be a Samurai in a Garden than a Gardener in the War',
+  'Don\'t keep right',
+  'Give me a break',
+  'That\'s all Folks!'
+];
