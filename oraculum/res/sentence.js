84de4f5e8495eccd1e0c06629b89ea6252c5642c
ganidilbert <124190842+ganidilbert@users.noreply.github.com>
2023-05-03 11:20:20 -0500

    Add files via upload

---
+++ oraculum/res/sentence.js
@@ -0,0 +1,26 @@
+var rats =
+[
+{
+'A great' :      ['A large','A small','No'],
+'treasure' :     ['horde','challenge','chance'],
+'awaits in' :    ['lies ahead','is expected','is anitcipated','surrounds'],
+'your' :         ['in ones','in their',' in no ones','in all'],
+'future' :       ['destiny','fate','past','present'],
+},
+
+{
+'You' :         ['You','I','They','Those'],
+'will' :        ['crave','give','leave','desire'],
+'need to be' :  ['a demand','a wish','the right','the use','the lack'],
+'cautious' :    ['of discreet','of tentative','of foolish','of idiotic','of responsible'],
+'today' :       ['attention','presence','deliverance','authority','conduct']
+},
+{
+'Danger' :          ['A crisis','An emergency','Great peril','Misfortune'],
+'is coming' :       ['awaits','halts','delays','expected','rushes'],
+'to those' :        ['to whom','to those','to ones','to many'],
+'who don/t' :       ['who ban','who desire','who strive','who negelct'],
+'see unclouded' :   ['clearly','uncertianty','obviously','vaguely'],
+}
+
+];
