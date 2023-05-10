94e04ef71aa523c1c97dfc900e3fc8a1ed3333a9
ganidilbert <124190842+ganidilbert@users.noreply.github.com>
2023-05-10 10:12:21 -0500

    Add files via upload

---
+++ oraculum/Fortune/res/sentence.js
@@ -0,0 +1,26 @@
+var rats =
+[
+{
+'A great' :      ['A large','A small','No','A great'],
+'treasure' :     ['horde','challenge','chance','treasure'],
+'awaits in' :    ['lies ahead','is expected','is anitcipated','surrounds','awaits in'],
+'your' :         ['in ones','in their',' in no ones','in all', 'your'],
+'future' :       ['destiny','fate','past','present','future'],
+},
+
+{
+'You' :         ['You','They','We',],
+'will' :        ['crave','give','leave','desire','will'],
+'need to be' :  ['a demand','a wish','the right','the use','the lack'],
+'cautious' :    ['of discreet','of tentative','of foolish','of idiotic','of responsible'],
+'today' :       ['attention','presence','deliverance','authority','conduct']
+},
+{
+'Danger' :          ['A crisis','An emergency','Great peril','Misfortune','Danger'],
+'is coming' :       ['awaits','halts','delays','expected','rushes','is coming'],
+'to those' :        ['to whom','to those','to ones','to many'],
+'who don/t' :       ['who ban','who desire','who strive','who negelct'],
+'see unclouded' :   ['clearly','uncertianty','obviously','vaguely'],
+}
+
+];
